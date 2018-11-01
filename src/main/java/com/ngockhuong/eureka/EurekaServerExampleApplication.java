d9fd041a3426b468347d9026aab88f55ef9b6033
Lam Ngoc Khuong <lam.ngoc.khuong@framgia.com>
2018-11-01 15:27:38 +0700

    init project

---
+++ src/main/java/com/ngockhuong/eureka/EurekaServerExampleApplication.java
@@ -0,0 +1,12 @@
+package com.ngockhuong.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerExampleApplication.class, args);
+    }
+}
